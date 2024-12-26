a3068104cce80f13ac4194d7f474ada2775e19c7
Jrtowers-prog <james.towers@students.plymouth.ac.uk>
2024-12-26 09:58:03 +0000

    Update MazeGame.cpp

--- MazeGame.cpp
+++ MazeGame.cpp
@@ -18,6 +18,14 @@ class Maze {
         int playerX, playerY;
         int rows = 15; //dimensions
         int cols = 15;
+        bool isValid (int x, int y){  //checks if cell is valid for carving (not outside paramters of map)
+              if (x >= 0 && x < rows && y >= 0 && y < cols && !visitedcells[x][y]){
+                return true;
+              }
+              else {
+                return false;
+              }
+            }
         
         void initialisemaze() {
             maze.resize(rows, vector<char>(cols, '#')); //initializes maze with dimensions cols and rows, also fills with #'s so that the carving algorithm works
@@ -99,51 +107,58 @@ class Maze {
         void carve() {
             int startX = 1; //carve start
             int startY = 1;
-            vector<vector<bool>> visited(rows, vector<bool>(cols, false)); //matrix for visited cells
+            vector<vector<bool>> visitedcells(rows, vector<bool>(cols, false)); //matrix for visited cells
             vector<pair<int, int>> directions = {{-2, 0}, {2, 0}, {0, -2}, {0, 2}}; //directions stored in this vector, done in 2's to make sure we skip over walls
             vector<pair<int, int>> walls; //surrounding walls from current position to be carved
-            bool isValid (int x, int y, vector<vector<bool>>& visited, int rows, int cols){  
-              if (x >= 0 && x < rows && y >= 0 && y < cols && !visited[x][y]){
-                return true;
-              }
-              else {
-                return false;
-              }
-            }
             maze[startX][startY] = ' ';
-            visited[startX][startY]= true;
+            visitedcells[startX][startY]= true;
 
-            for (int i = o; i < directions.size(); i++){
+            for (int i = 0; i < directions.size(); i++){  //adding surrounding walls to walls list
                 int wallX = startX + directions[i].first;
                 int wallY = startY + directions[i].second;
-                if (wallX < rows && wallX > 0 && wallY > 0 && wallY < cols){
+                if (wallX < rows && wallX > 0 && wallY > 0 && wallY < cols){  //checking if surrounding walls are valid before adding to list
                     walls.push_back({wallX,wallY});
                 }
             }
 
-            while (!(walls.length() == 0)){
-                int carvechoice = rand() % walls.size();
+            while (!(walls.size() == 0)){ //loop iterates until surrounding walls are all used
+                int carvechoice = rand() % walls.size(); //pick number from 1 to number of surrounding walls
+                pair<int,int> wallchoice = walls[carvechoice]; //chosen wall
+                walls[carvechoice]= walls.back(); //replacing random choice with back element them popping back element in next line to effectively remove the chosen wall from the walls list as it is now going to be carved.
+                walls.pop_back(); //swap and pop technique eliminates chosen wall from wall list as it has been chosen.
+
+                int wallX = wallchoice.first; //setting coordinates of the randomly selected wall
+                int wallY = wallchoice.second;
                 
+                for (int i = 0; i < directions.size(); i++) { //loop iterating through up, down, left, right options.
+                    int nextX = wallX + directions[i].first / 2; //divide by 2 as we are finding the cells position relative from the position of the wall. The cell is immediately next to a wall and therefore we don't jump by 2 but by 1 and have to divide the direction vector by 2.
+                    int nextY = wallY + directions[i].second / 2;
+
+                    if (isValid(nextX,nextY)){ //checking next cells validity
+                        maze[nextX][nextY]=' '; //carving
+                        maze[wallX][wallY]=' ';
+                        visitedcells[nextX][nextY]=true; //add to visited list
+                    }
+
+                     for (int j = 0; j < directions.size(); j++) {
+                         int nextWallX = nextX + directions[j].first;
+                         int nextWallY = nextY + directions[j].second;
+                         if (nextWallX > 0 && nextWallX < rows - 1 && nextWallY > 0 && nextWallY < cols - 1) {
+                             walls.push_back({nextWallX, nextWallY});
+                         }
+                     }
+                }
             }
+           
+
+            
 
             
 
         }
 };
 
-class Enemy {
-    public:
-        char enemystart();
-       
-};
 
-class Puzzle {
-    public:
-        char placekey();
-        char placedoor();
-        bool haskey();
-
-};
 
 int main() {
     Maze gameMaze;
